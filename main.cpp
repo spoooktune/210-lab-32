4198b06ad1b0fd21645c850cf4346901241629ed
spoooktune <asriprasana5@gmail.com>
2025-11-14 21:32:23 -0800

    Milestone 2: system loops until carQ is empty

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Car.h"
 
 using namespace std;
+const int PAYS_TOLL = 55;
 
 int main(){
     deque<Car> carQ;
@@ -15,5 +16,25 @@ int main(){
         temp.print();
     }
     cout << endl;
+    int r = 0;
+    while (!carQ.empty()){
+        r = rand() % 100 + 1;
+        if (r <= PAYS_TOLL){
+            cout << "Car paid: ";
+            carQ.front().print();
+            carQ.pop_front();
+        }
+        else if (r > PAYS_TOLL){
+            Car temp;
+            cout << "Car joined lane: ";
+            temp.print();
+            carQ.push_back(temp);
+        }
+        cout << "Queue: " << endl;
+        for (Car c : carQ){
+            cout << "> ";
+            c.print();
+        }
+    }
     return 0;
 }
