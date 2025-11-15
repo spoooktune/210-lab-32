683b5015d12e1e0b5d42bf73639bc48e6c8b72be
spoooktune <asriprasana5@gmail.com>
2025-11-14 21:18:41 -0800

    Milestone 1: for loop that populates carQ w/ 2 Car objects

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <deque>
+#include <random>
 #include "Car.h"
 
 using namespace std;
 
 int main(){
+    deque<Car> carQ;
+    cout << "Initial Queue: " << endl;
+    for (int i = 0; i < 2; i++){
+        Car temp;
+        carQ.push_back(temp);
+        cout << "> ";
+        temp.print();
+    }
+    cout << endl;
     return 0;
 }
