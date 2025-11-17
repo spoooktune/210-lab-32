ba1dd63dd5873fc09536f6a70d29091b1a178969
spoooktune <asriprasana5@gmail.com>
2025-11-16 17:16:10 -0800

    Started coding for loop for each period

--- main.cpp
+++ main.cpp
@@ -24,24 +24,24 @@ int main(){
         }
     }
     cout << endl;
-    /*
     int r = 0;
-    int numPeriods = 0;
-    while (!carQ.empty()){
-        numPeriods++;
-        cout << "Period " << numPeriods << endl;
-        r = rand() % 100 + 1;
-        if (r <= PAYS_TOLL){
-            cout << "Car paid: ";
-            carQ.front().print();
-            carQ.pop_front();
+    for (int i = 0; i < 20; ){
+        cout << "Period " << i + 1 << endl;
+        for (int x = 0; x < 4; x++){
+            r = rand() % 100 + 1;
+            if (r <= PAYS_TOLL){
+                cout << "Car paid: ";
+                carQ.front().print();
+                carQ.pop_front();
+            }
+            else if (r > PAYS_TOLL){
+                Car temp;
+                cout << "Car joined lane: ";
+                temp.print();
+                carQ.push_back(temp);
+            }
         }
-        else if (r > PAYS_TOLL){
-            Car temp;
-            cout << "Car joined lane: ";
-            temp.print();
-            carQ.push_back(temp);
-        }
+        /*
         cout << "Queue: " << endl;
         if (carQ.empty()){
             cout << "> Queue is empty" << endl;
@@ -53,6 +53,7 @@ int main(){
             }
         }
         cout << endl;
-    }*/
+    }
+    //*/
     return 0;
 }
