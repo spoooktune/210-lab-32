05212dd43d6afb3a0edc0e48f1154f71edd6cb48
spoooktune <asriprasana5@gmail.com>
2025-11-16 17:39:32 -0800

    Milestone 4: Cars can join/leave each lane

--- main.cpp
+++ main.cpp
@@ -14,35 +14,36 @@ int main(){
     
     cout << "Initial Queue: " << endl;
     for (int x = 0; x < 4; x++){
-        deque<Car> carQ = carPlaza[x];
+        deque<Car> * carQ = &carPlaza[x];
         cout << "> Lane " << x + 1 << endl;
         for (int i = 0; i < 2; i++){
             Car temp;
-            carQ.push_back(temp);
+            carQ->push_back(temp);
             cout << "   ";
             temp.print();
         }
     }
     cout << endl;
     int r = 0;
-    for (int i = 0; i < 20; ){
+    for (int i = 0; i < 20; i++){
         cout << "Period " << i + 1 << endl;
         for (int x = 0; x < 4; x++){
             cout << "> Lane " << x + 1 << endl;
-            deque<Car> carQ = carPlaza[x];
+            deque<Car> * carQ = &carPlaza[x];
             r = rand() % 100 + 1;
-            if (r <= PAYS_TOLL){
+            if (r <= PAYS_TOLL && !carQ->empty()){
                 cout << "   Car paid: ";
-                carQ.front().print();
-                carQ.pop_front();
+                carQ->front().print();
+                carQ->pop_front();
             }
             else if (r > PAYS_TOLL){
                 Car temp;
                 cout << "   Car joined lane: ";
-                temp.print();
-                carQ.push_back(temp);
+                carQ->push_back(temp);
+                carQ->back().print();
             }
         }
+        cout << endl;
         
         cout << "Queue: " << endl;
         for (int x = 0; x < 4; x++){
