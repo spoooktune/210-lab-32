89c0f14c1e0cfb0e98bf23c2f1ddc1c7933adb1f
spoooktune <asriprasana5@gmail.com>
2025-11-16 17:02:31 -0800

    Milestone 3: each lane is populated w/ 2 cars, queue status is shown

--- main.cpp
+++ main.cpp
@@ -9,24 +9,22 @@ const int PAYS_TOLL = 55;
 
 int main(){
     srand(time(0));
-    // testing if array of deques functions
+    // array of deques
     array<deque<Car>, 4> carPlaza;
-    Car temp;
-    carPlaza[0].push_back(temp);
-    carPlaza[0].front().print();
-    Car temp2;
-    carPlaza[1].push_back(temp2);
-    carPlaza[1].front().print();
-
-    /*
+    
     cout << "Initial Queue: " << endl;
-    for (int i = 0; i < 2; i++){
-        Car temp;
-        carQ.push_back(temp);
-        cout << "> ";
-        temp.print();
+    for (int x = 0; x < 4; x++){
+        deque<Car> carQ = carPlaza[x];
+        cout << "> Lane " << x + 1 << endl;
+        for (int i = 0; i < 2; i++){
+            Car temp;
+            carQ.push_back(temp);
+            cout << "   ";
+            temp.print();
+        }
     }
     cout << endl;
+    /*
     int r = 0;
     int numPeriods = 0;
     while (!carQ.empty()){
